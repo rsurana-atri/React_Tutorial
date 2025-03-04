2914f1c6be3b6473c94f2c8bd4121b76c436d7a9
rsurana-atri <rsurana@atrihub.io>
2025-03-04 10:04:31 -0800

    feat(data routing): Test routing
    Implement sample data passing thgrough the website and into the HTML page, test webpage on vite
    
    [ICTRAINING-1873]

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,11 +2,13 @@ import AppRouter from "./routes/AppRouter";
 import RecipeProvider from "./context/RecipeProvider";
 
 const App: React.FC = () => {
+// basic recipe provider
   return (
     <RecipeProvider>
       <AppRouter />
     </RecipeProvider>
   );
+
 };
 
 export default App;
